4078faaf40dcc930a1457d6486e064d1dc4d2205
mayurtechs <maddydiwan@gmail.com>
2022-08-16 04:07:40 -0700

    second commit added

--- src/git_test/Test.java
+++ src/git_test/Test.java
@@ -3,5 +3,6 @@ package git_test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("In the git test");
+	System.out.println("Successfully pushed git repository");
 }
 }
