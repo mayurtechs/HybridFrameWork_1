1056e791052ed451af79387c4d630ad6fc0ccaf4
mayurtechs <86462635+mayurtechs@users.noreply.github.com>
2022-11-01 03:44:01 -0700

    Pull request done  from eclipse forTest1

--- src/git_test/Test1.java
+++ src/git_test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 public static void main(String[]args)
 {
   System.out.println("Pull from github to eclipse ");
+  System.out.println("Pull request from eclipse to github");
 }
 }
