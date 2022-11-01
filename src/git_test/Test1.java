fcc400dd84321b4a71f54f0fcf5d25392b243e6b
mayurtechs <86462635+mayurtechs@users.noreply.github.com>
2022-11-01 03:36:31 -0700

    Updated Test1

--- src/git_test/Test1.java
+++ src/git_test/Test1.java
@@ -3,6 +3,6 @@ package git_test;
 public class Test1 {
 public static void main(String[]args)
 {
-  System.out.println("Pull from github");
+  System.out.println("Pull from github to eclipse ");
 }
 }
