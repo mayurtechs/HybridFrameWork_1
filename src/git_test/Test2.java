4d3c9cc2703783aeb8b55235bef0ee66dd0f050f
mayurtechs <maddydiwan@gmail.com>
2022-11-01 04:01:43 -0700

    added to Test2

--- src/git_test/Test2.java
+++ src/git_test/Test2.java
@@ -2,6 +2,6 @@ package git_test;
 public class Test2 {
   public static void main(String[] args)
   {
-	  System.out.println("Push to github repository");
+	  System.out.println("Push to github repository ");
   }
 }
